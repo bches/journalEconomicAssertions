312f5084f7f5dafcb678cc7a2e019b6901b72eac
bches <bpchesney@gmail.com>
2023-02-15 12:59:37 -0600

    updated tests for economic assertions

--- test/test_economicAssertion.py
+++ test/test_economicAssertion.py
@@ -4,22 +4,13 @@ from JEAdata import economicAssertion
 
 
 if __name__ == '__main__':
-    filename = '../accounting/DiscountBondPurchase.csv'
+    purchase = economicAssertion(filename = '../accounting/DiscountBondPurchase.csv')
+    purchase.addInputVariable('iM')
+    print(purchase)
 
-    if 'write' in sys.argv:
-        with open(filename, 'w', newline='') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            spamwriter.writerow(['Input Variables:', 'issuer', 'holder', 'FV', 'PR', 'T', 
-                                 'Cash', 'Discount on Bond Payable', 'Bond Payable', 
-                                 'Discount on Bond Investment', 'Bond Investment'])
-            spamwriter.writerow(['issuer','DR','CR','holder','DR','CR'])
-            spamwriter.writerow(['Cash','graphPoint(PR,T)','','Cash','','graphPoint(PR,T)'])
-            spamwriter.writerow(['Discount on Bond Payable','graphPoint(FV-PR,T)','','Discount on Bond Investment','','graphPoint(FV-PR,T)'])
-            spamwriter.writerow(['Bond Payable','','graphPoint(FV,T)','Bond Investment','graphPoint(FV,T)',''])
+    payments = economicAssertion(filename = '../accounting/DiscountBondPeriodicPayment.csv')
+    payments.addInputVariable('iSR')
+    print(payments)
 
-
-
-    dut = economicAssertion(filename = filename)
-
-    print(dut)
+    repayment = economicAssertion(filename = '../accounting/DiscountBondRepayment.csv')
+    print(repayment)
