2ed9ff9fef3d49db97da8023f11c7d214d2b7c45
bches <bpchesney@gmail.com>
2023-02-20 21:02:25 -0600

    fixed the amortization of the discount in a bond

--- src/assertions/bonds.py
+++ src/assertions/bonds.py
@@ -1,6 +1,6 @@
 # Economic assertions associated with bonds
 
-from JEAdata.JEAdata import economicAssertion
+from JEAbooks.JEAbooks import economicAssertion
 from tools.tvom import pv, pva
 
 class bond(economicAssertion):
@@ -8,7 +8,9 @@ class bond(economicAssertion):
   bond purchased at a discoiunt, premium or face vale.
   Derived from economicAssertion base class.'''
 
-  def __init__(self, issuer, FV, T, N, L, iSR, accounts, dir = '../../accounting'):
+  def __init__(self, issuer,
+               FV, T, N, L, iSR,
+               accounts, dir = '../../accounting'):
     '''issuer issues a bond to be purchased
     FV: Face value of the bond per tranche
     T: number of tranches purchased
@@ -33,7 +35,7 @@ class bond(economicAssertion):
     self.bondType = None
     self.accounts = accounts
 
-  def __call__(self, k, holder, iM, Tp):
+  def __call__(self, G, k, holder, iM, Tp):
     '''The bond is purchased and the issuer and holder jointly attest to the
     economic transaction of the bond.  All journal entries associated with the
     life cycle of the bond are recorded for both parties.
@@ -42,32 +44,44 @@ class bond(economicAssertion):
     FV = self.args['FV']
     iSR = self.args['iSR']
     N = self.args['N']
-    P = FV * iSR
-    PR = FV * pv(iM, N) + P * pva(iM, N)
-
+    PA = FV * iSR
+    PR = FV * pv(iM, N) + PA * pva(iM, N)
+    BV = PR
+    
     args = {}
     args.update(self.args)
     args.update(self.accounts)
     args.update({'holder':holder, 'T':Tp, 'PR':PR})
     if FV > PR:
+      print('\tDiscount Bond')
       bondType = 'Discount'
-      self.discount['purchase'](k=k, args=args)
+      print('\tExecuting purchase assertion...')
+      print('\tassertion = %s\n' % self.discount['purchase'])
+      self.discount['purchase'](G=G, k=k, args=args)
       for i in range(N):
-        self.discount['payments'](k=k, args=args)
-      self.discount['repayment'](k=k, args=args)
+        print('\n\tExecuting purchase assertion (%d)...' % i)
+        interest = iM * BV
+        AM = interest - PA
+        BV += AM
+        print('(interest, BV, AM)=%s,%s,%s' % (interest, BV, AM))
+        print('\tassertion = %s\n' % self.discount['payments'])
+        args['AM'] = AM
+        args['PA'] = PA
+        self.discount['payments'](G=G, k=k+i+1, args=args)
+      self.discount['repayment'](G=G, k=k+N-1, args=args)
     elif PR > FV:
       bondType = 'Premium'
-      self.premium['purchase'](k=k, args=args)
+      self.premium['purchase'](G=G, k=k, args=args)
       for i in range(N):
-        self.premium['payments'](k=k, args=args)
-      self.premium['repayment'](k=k, args=args)
+        self.premium['payments'](G=G, k=k, args=args)
+      self.premium['repayment'](G=G, k=k, args=args)
     else:
       bondType = 'Face'
-      self.face['purchase'](k=k, args=args)
+      self.face['purchase'](G=G, k=k, args=args)
       for i in range(N):
-        self.face['payments'](k=k, args=args)
-      self.face['repayment'](k=k, args=args)
+        self.face['payments'](G=G, k=k, args=args)
+      self.face['repayment'](G=G, k=k, args=args)
 
-    # decrement tranches available
+    # decrement number of tranches available
     self.T -= Tp
       
--- test/test_bonds.py
+++ test/test_bonds.py
@@ -1,10 +1,16 @@
 import sys
 sys.path.append('../src')
 from assertions import bond 
-
+from JEAbooks.JEAbooks import JEAbooks
 
 if __name__ == '__main__':
-    dut = bond(issuer=1, FV=60e3, T=7, N=5, L=2, iSR=0.05,
+    # issuer creates the intent to enter into a bond agreement.
+    # The bond is split into 10 tranches of 100,000 each.
+    # The bond lasts for 6 periods, with no time in between
+    # periods (L=0).  The stated rated of the 6% per period. 
+    print('At issue:')
+    N = 6
+    dut = bond(issuer=1, FV=100e3, T=10, N=N, L=0, iSR=0.06,
                accounts = {'Cash':1, 'Bond Payable':2,
                            'Bond Investment':3,
                            'Discount on Bond Payable':4,
@@ -12,7 +18,22 @@ if __name__ == '__main__':
                            'Interest Expense':6,
                            'Interest Revenue':7},
                dir='../accounting')
-    print(dut)
+    print(dut,'\n')
 
-    dut(k=0, holder=2, iM=0.06, Tp=3)
-    print(dut)
+    # create the books for the JEA record keeping
+    books = JEAbooks(Nperiods=10,
+                     acctMap_filename='../accounting/accountMapping.csv')
+    
+    # holder purchases 7 tranches of the bond when the market
+    # rate is 7% perperiod.
+    print('At purchase:')
+    dut(G=books.G, k=0, holder=2, iM=0.07, Tp=7)
+
+    # check the books
+    print()
+    print('books.G =', books.G)
+
+    # amortization:
+    print('\n amortization:')
+    for i in range(1,N+1):
+        print('\t%d: %s' % (i, books.G[i]))
